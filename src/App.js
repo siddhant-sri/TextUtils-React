7602af9219d75c07d72611b04e97adb3b5c3f506
siddhant-sri <sidz.dec26@gmail.com>
2023-05-18 22:24:25 +0530

    Merge branch 'master' of https://github.com/siddhant-sri/textutils

--- src/App.js
+++ src/App.js
@@ -10,10 +10,20 @@ import Alert from "./Components/Alert";
 //   Route
 // } from "react-router-dom";
 
+const removeBodyClasses = ()=>{
+  document.body.classList.remove("bg-light");
+  document.body.classList.remove("bg-success");
+  document.body.classList.remove("bg-warning");
+  document.body.classList.remove("bg-danger");
+  document.body.classList.remove("bg-primary");
+}
+
 
 function App() {
   const [mode, setMode] = useState('light')
-  const toggleMode = () =>{
+  const toggleMode = (cls) =>{
+    removeBodyClasses();
+    document.body.classList.add("bg-" + cls);
     if(mode === "light"){
       setMode("dark")
       document.body.style.backgroundColor = "#042743"
@@ -26,19 +36,6 @@ function App() {
       showAlert("Light Mode is enabled", "success")
       document.title = "Textutils - Light mode"
     }
-  }
-  const greenToggleMode = () =>{
-    if(mode === "light"){
-      setMode("dark")
-      document.body.style.backgroundColor = "#56a157"
-      showAlert("Green Mode is enabled", "success")
-    }
-    else{
-      setMode("light")
-      document.body.style.backgroundColor = "white"
-      showAlert("Light Mode is enabled", "success")
-    }
-
   }
 
   const [alert, setAlert] = useState(null)
@@ -55,7 +52,7 @@ function App() {
   return (
     <>
       {/* <Router> */}
-        <NavBar title="Textutils" mode={mode} greenToggleMode={greenToggleMode} toggleMode={toggleMode}/>
+        <NavBar title="Textutils" mode={mode} toggleMode={toggleMode}/>
         <Alert alert={alert}/>
         <div className="container my-3">
           {/* <Routes> */}
